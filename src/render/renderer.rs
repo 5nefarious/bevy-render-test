4de9bcf521962f1ec81af3738a9d1b8d1eca767c
Arvin Ignaci <arvin.ignaci@gmail.com>
2021-03-25 16:51:37 -0400

    Switch to linear filtering.

--- src/render/renderer.rs
+++ src/render/renderer.rs
@@ -123,7 +123,7 @@ impl Renderer {
             address_mode_u: wgpu::AddressMode::ClampToEdge,
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
-            mag_filter: wgpu::FilterMode::Nearest,
+            mag_filter: wgpu::FilterMode::Linear,
             min_filter: wgpu::FilterMode::Linear,
             mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
