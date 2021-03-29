72ad33622916eb7e5cabb1274a4500c80703bb03
Arvin Ignaci <arvin.ignaci@gmail.com>
2021-03-29 18:12:49 -0400

    Make dispatcher an exclusive system.

--- src/render/plugin.rs
+++ src/render/plugin.rs
@@ -10,7 +10,7 @@ pub struct RenderPlugin;
 impl Plugin for RenderPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.insert_resource(Dispatcher::new())
-            .add_system(dispatcher_system.system())
+            .add_system(dispatcher_system.exclusive_system())
             .add_system(render_system.system());
     }
 }
